6379eae94c5363a293d9c53a16f3d9c07c02e9c9
Thiago <thiagosales06@hotmail.com>
2021-06-24 21:09:34 -0300

    atualizando

---
+++ node/instanciaUnica.js
@@ -0,0 +1,7 @@
+// node faz cahce
+module.exports = {
+    valor: 1,
+    inc() {
+        this.valor++
+    }
+}
---
+++ node/moduloA.js
@@ -0,0 +1,3 @@
+this.ola = 'Olá Mundo!'
+exports.bemVindo = '\nBem vindo ao node!'
+module.exports.ateLogo = '\nAté o próximo exemplo\n'
