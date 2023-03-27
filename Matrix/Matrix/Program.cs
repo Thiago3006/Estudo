f33379d5ad5e396cff73055c74a2e9c2fb396d3b
ThiagoAlvesDev <thiagosales06@hotmail.com>
2023-03-27 19:46:55 -0300

    Add Matrix

---
+++ Matrix/Matrix/Program.cs
@@ -0,0 +1,60 @@
+﻿using System;
+
+namespace Matrix
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] ent = Console.ReadLine().Split(' ');
+            int n = int.Parse(ent[0]);
+            int m = int.Parse(ent[1]);
+
+            int[,] mat = new int[n, m];
+
+
+
+
+            for (int i = 0; i < n; i++)
+            {
+                string[] values = Console.ReadLine().Split(' ');
+                for (int j = 0; j < m; j++)
+                {
+                    mat[i, j] = int.Parse(values[j]);
+                }
+            }
+
+            int ale = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < n; i++)
+            {
+
+                for (int j = 0; j < m; j++)
+                {
+                    if (mat[i, j] == ale)
+                    {
+                        Console.WriteLine($"Position {i},{j}:");
+                        if (j > 0)
+                        {
+                            Console.WriteLine($"Left: {mat[i, j - 1]}");
+                        }
+                        if (i > 0)
+                        {
+                            Console.WriteLine($"Up: {mat[i - 1, j]}");
+                        }
+                        if( j < n - 1)
+                        {
+                            Console.WriteLine($"Right: {mat[i, j + 1]}");
+                        }
+                        if (i < m - 1)
+                        {
+                            Console.WriteLine($"Down: {mat[i + 1, j]}");
+                        }
+                    }
+
+                }
+            }
+
+        }
+    }
+}
---
+++ Matrix/README.md
@@ -0,0 +1,48 @@
+## Proposal 
+
+Make a program to read two integers M and N, and then read
+an array of M rows by N columns containing integers,
+there may be repetitions. Then read an integer X that
+belongs to the headquarters. For each instance of X, show the values to
+left, above, right, and below X, if any, as
+example
+
+<hr/>
+
+## Example
+
+3 4
+
+10 8 15 12
+
+21 11 23 8
+
+14 5 13 19
+
+8
+
+Position 0,1:
+
+Left: 10
+
+Right: 15
+
+Down: 11
+
+Position 1,3:
+
+
+Left: 23
+
+Up: 12
+
+Down: 19
+
+
+
+
+## To execute
+click on the solution if you have visual studio installed, if you intend to use vsCode open the folder, enter the terminal, open SalaryControl and run dotnet run.
+
+
+
