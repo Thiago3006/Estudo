da5f97b1e802fefd5acaa8f49de0051eef481c50
ThiagoAlvesDev <thiagosales06@hotmail.com>
2023-03-29 11:02:13 -0300

    Add instruction  phrases

--- Matrix/Matrix/Program.cs
+++ Matrix/Matrix/Program.cs
@@ -6,54 +6,46 @@ namespace Matrix
     {
         static void Main(string[] args)
         {
-            string[] ent = Console.ReadLine().Split(' ');
-            int n = int.Parse(ent[0]);
-            int m = int.Parse(ent[1]);
+            Console.Write("Enter two separate numbers where the first determines the row and the second the column: ");
+            string[] s = Console.ReadLine().Split(' ');
+            int m = int.Parse(s[0]);
+            int n = int.Parse(s[1]);
+            int[,] mat = new int[m,n];
+            Console.WriteLine($"\nNow below, inform the set of {n} numbers separating by space and press enter, this process will be repeated {m} times:");
 
-            int[,] mat = new int[n, m];
-
-
-
-
-            for (int i = 0; i < n; i++)
-            {
-                string[] values = Console.ReadLine().Split(' ');
-                for (int j = 0; j < m; j++)
-                {
-                    mat[i, j] = int.Parse(values[j]);
+            for(int i =0; i < m; i++){
+                string[] value = Console.ReadLine().Split(' ');
+                for(int j = 0; j < n; j++){
+                    mat[i,j] = int.Parse(value[j]);
                 }
             }
 
-            int ale = int.Parse(Console.ReadLine());
+            Console.Write("\nEnter with value to verificate your values in around his: ");
+            int u = int.Parse(Console.ReadLine());
 
-            for (int i = 0; i < n; i++)
-            {
+            for(int i = 0; i < m; i++){
+                for(int j = 0; j < n; j++){
+                    if(mat[i,j] == u){
+                        Console.WriteLine($"\nPosition: {i},{j}");
+                        if(i > 0){
+                            Console.WriteLine($"Up: {mat[i-1, j]}");
+                        }
+                        if(j < n -1){
+                            Console.WriteLine($"Right: {mat[i, j+1]}");
+                        }
+                        if(i < m -1){
+                            Console.WriteLine($"Down: {mat[i+1, j]}");
+                        }
+                        if(j > 0){
+                            Console.WriteLine($"Left: {mat[i, j -1]}");
+                        }
 
-                for (int j = 0; j < m; j++)
-                {
-                    if (mat[i, j] == ale)
-                    {
-                        Console.WriteLine($"Position {i},{j}:");
-                        if (j > 0)
-                        {
-                            Console.WriteLine($"Left: {mat[i, j - 1]}");
-                        }
-                        if (i > 0)
-                        {
-                            Console.WriteLine($"Up: {mat[i - 1, j]}");
-                        }
-                        if( j < n - 1)
-                        {
-                            Console.WriteLine($"Right: {mat[i, j + 1]}");
-                        }
-                        if (i < m - 1)
-                        {
-                            Console.WriteLine($"Down: {mat[i + 1, j]}");
-                        }
                     }
-
                 }
             }
+
+
+
 
         }
     }
