f452fa5f78bbe11f53b271c0cfc5d336c97d375d
ThiagoAlvesDev <thiagosales06@hotmail.com>
2023-04-11 15:47:15 -0300

    Conceitos de string builder

---
+++ StringBuilder/StringBuilder/Entities/Post.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using StringB.Entities;
+
+namespace StringB.Entities
+{
+    internal class Post
+    {
+        public DateTime Date { get; set; }
+        public string Title { get; set; }
+        public string Content { get; set; }
+        public int Likes { get; set; }
+        public List<Comment> Comments{ get; set; } = new List<Comment>();
+
+        public Post()
+        {
+
+        }
+
+        public Post(DateTime date, string title, string content, int like)
+        {
+            Date = date;
+            Title = title;
+            Content = content;
+            Likes = like;
+        }
+
+        public void AddComment(Comment comment)
+        {
+            Comments.Add(comment);
+        }
+
+        public void RemoveComment(Comment comment)
+        {
+            Comments.Remove(comment);
+        }
+
+        public override string ToString()
+        {
+            StringBuilder sb = new StringBuilder();
+            sb.AppendLine(Title);
+            sb.Append(Likes);
+            sb.Append(" Likes - ");
+            sb.AppendLine(Date.ToString("dd/MM/yyyy HH:mm:ss"));
+            sb.AppendLine(Content);
+            sb.AppendLine("Comments: ");
+            foreach(Comment comment in Comments)
+            {
+                sb.AppendLine(comment.Text);
+            }
+            return sb.ToString();
+        }
+
+    }
+}
