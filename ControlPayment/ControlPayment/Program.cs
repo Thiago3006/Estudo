7f0193fc1c0ad678f9a9166faa3b81d2796c9ecf
ThiagoAlvesDev <thiagosales06@hotmail.com>
2023-04-24 17:02:17 -0300

    Herança e Polimorfimos

---
+++ ControlPayment/ControlPayment/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using ControlPayment.Entities;
+using System.Globalization;
+
+namespace ControlPayment
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Employee> list = new List<Employee>();
+            Console.Write("Enter the number of employess: ");
+            int n = int.Parse(Console.ReadLine());
+
+            for (int i = 1; i <= n; i++)
+            {
+                Console.WriteLine($"\nEmployee #{i} data:");
+                Console.Write("OutSourced (y/n): ");
+                char q = char.Parse(Console.ReadLine());
+                Console.Write("Name: ");
+                string name = Console.ReadLine();
+                Console.Write("Hours: ");
+                int hours = int.Parse(Console.ReadLine());
+                Console.Write("Value per hour: ");
+                double value = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+                if (q == 'y')
+                {
+                    Console.Write("Additional charge: ");
+                    double addval = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+                    list.Add(new OutsourcedEmployee(name, hours, value, addval));
+                }
+                if (q == 'n')
+                {
+                    list.Add(new Employee(name, hours, value));
+                }
+
+            }
+
+            Console.WriteLine($"\nPAYMENTS:");
+            foreach (Employee e in list)
+            {
+                Console.Write(e);
+            }
+        }
+    }
+}
