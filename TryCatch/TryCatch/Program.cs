6886189fa6fe131a5d6d583938f10f9f66a9699d
ThiagoAlvesDev <thiagosales06@hotmail.com>
2023-04-28 19:08:31 -0300

    Try Catch

---
+++ TryCatch/TryCatch/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace TryCatch
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                int a = int.Parse(Console.ReadLine());
+                int b = int.Parse(Console.ReadLine());
+
+                Console.WriteLine(a / b);
+            }
+
+            catch (DivideByZeroException)
+            {
+                Console.WriteLine("It is not divisible by zero!");
+            }
+            catch(FormatException e)
+            {
+                Console.WriteLine("Icorrect Format!" + e.Message);
+            }
+
+        }
+    }
+}
