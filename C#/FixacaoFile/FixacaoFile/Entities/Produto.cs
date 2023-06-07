c3e1592f298a309de26e9ecf67bb9cefbf5bdc0e
Thiago Alves <thiagosales06@hotmail.com>
2023-06-07 14:02:47 -0300

    Resolvendo problema sobre o tema File

---
+++ C#/FixacaoFile/FixacaoFile/Entities/Produto.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FixacaoFile.Entities
+{
+    internal class Produto
+    {
+        string Name { get; set; }
+        double Price { get; set; }
+        int Quantity { get; set; }
+
+        public Produto(string name, double price, int quantity)
+        {
+            Name = name;
+            Price = price;
+            Quantity = quantity;
+        }
+
+        public double  Total()
+        {
+            return Quantity* Price;
+        }
+    }
+}
---
+++ C#/FixacaoFile/FixacaoFile/Program.cs
@@ -0,0 +1,55 @@
+﻿using FixacaoFile.Entities;
+using System;
+using System.Globalization;
+using System.IO;
+
+namespace FixacaoFile
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Insira o caminho para o arquivo: ");
+            string pasta = Console.ReadLine();
+
+
+
+            string[] lines = File.ReadAllLines(pasta);
+
+            string dentroPasta = Path.GetDirectoryName(pasta);
+            string novaPasta = dentroPasta + @"\out";
+            string novoArquivo = novaPasta + @"\summary.csv";
+
+            Directory.CreateDirectory(novaPasta);
+
+            try
+            {
+
+
+                using (StreamWriter sw = File.AppendText(novoArquivo))
+                {
+                    foreach (string line in lines)
+                    {
+                        string[] v = line.Split(',');
+                        string name = v[0];
+                        double price = double.Parse(v[1], CultureInfo.InvariantCulture);
+                        int quantity = int.Parse(v[2]);
+
+                        Produto produto = new Produto(name, price, quantity);
+
+                        sw.WriteLine($"{name}, {produto.Total().ToString("F2", CultureInfo.InvariantCulture)}");
+
+
+                    }
+                }
+            }
+            catch (IOException e)
+            {
+                Console.WriteLine("Um erro ocorreu" + e.Message);
+            }
+        }
+
+
+    }
+}
+
