388c069bf07d7639378b5e38e25700e1daef7566
Thiago Alves <thiagosales06@hotmail.com>
2023-07-22 16:44:54 -0300

    Intarface

---
+++ C#/CarRent/CarRent/Entitites/Invoice.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Globalization;
+
+namespace CarRent.Entitites
+{
+    internal class Invoice
+    {
+        public double BasicPayment { get; set; }
+        public double Tax { get; set; }
+        public double TotalPayment { get; set; }
+
+        public Invoice (double basicPayment, double tax, double totalPayment)
+        {
+            BasicPayment = basicPayment;
+            Tax = tax;
+            TotalPayment = totalPayment;
+        }
+
+        public override string ToString()
+        {
+            return $"Basic Payment: {BasicPayment.ToString("F2", CultureInfo.InvariantCulture)}" +
+                $"\nTax: {Tax.ToString("F2", CultureInfo.InvariantCulture)}" +
+                $"\nTotal Payment: {TotalPayment.ToString("F2", CultureInfo.InvariantCulture)}";
+        }
+    }
+}
---
+++ C#/CarRent/CarRent/Services/RentalService.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CarRent.Services
+{
+    internal class RentalService
+    {
+        public double PriceHour { get; set; }
+        public double PriceDay { get; set; }
+
+        public 0
+
+    }
+}
