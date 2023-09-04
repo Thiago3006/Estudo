de56dce21cb5b868bfdba16a71d9265a5eb36fca
Thiago Alves <thiagosales06@hotmail.com>
2023-09-04 08:08:39 -0300

    Criação das entidades

---
+++ C#/ContratosProcess/ContratosProcess/Entities/contract.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ContratosProcess.Entities
+{
+    internal class contract
+    {
+        public int ContractNumber { get; set; }
+        public DateTime Date { get; set; }
+        public double Value { get; set; }
+        public List<Installments> Parts { get; set; }
+
+
+        public contract (int contractnumber, DateTime date, double value)
+        {
+            ContractNumber = contractnumber;
+            Date = date;
+            Value = value;
+            Parts = new List<Installments>();
+        }
+
+        public void AddInstallment(Installments installments)
+        {
+
+        }
+    }
+}
---
+++ C#/ContratosProcess/ContratosProcess/Program.cs
@@ -0,0 +1,9 @@
+﻿namespace ContratosProcess
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
