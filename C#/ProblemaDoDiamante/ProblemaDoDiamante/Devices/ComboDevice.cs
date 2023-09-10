8e1e2b6166cc5ec750f7419e3b2aa0d57476d384
Thiago Alves <thiagosales06@hotmail.com>
2023-09-10 17:00:02 -0300

    Problema do diamente, herança multipla

---
+++ C#/ProblemaDoDiamante/ProblemaDoDiamante/Devices/ComboDevice.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProblemaDoDiamante.Devices
+{
+    internal class ComboDevice : Device, IPrinter, IScanner
+    {
+        public void Print(string document)
+        {
+            Console.WriteLine($"ComboDevice print {document}"); 
+        }
+
+        public override void ProcessDoc(string document)
+        {
+            Console.WriteLine($"ComboDevice processing: {document}");
+        }
+
+        public string Scan()
+        {
+            return "ComboDevice scan result";
+        }
+    }
+}
+
+
+//Criado interfaces para que possa ser usado o mesmo metodo usado na classe 'Printer' e 'Scanner' (ProcessDoc)
---
+++ C#/ProblemaDoDiamante/ProblemaDoDiamante/Program.cs
@@ -0,0 +1,25 @@
+﻿using ProblemaDoDiamante.Devices;
+
+namespace ProblemaDoDiamante
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Printer p = new Printer() { SerialNumber = 1080 };
+            p.ProcessDoc("My letter");
+            p.Print("My letter");
+
+
+            Scanner s = new Scanner() { SerialNumber = 2003 };
+            s.ProcessDoc("An Email");
+            Console.WriteLine(s.Scan());
+
+            ComboDevice c = new ComboDevice() { SerialNumber = 1023 };
+            c.ProcessDoc("ComboDevice");
+            c.Print("ComboDevice");
+            Console.WriteLine(c.Scan());
+        }
+    }
+}
