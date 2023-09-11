797d195a1dd4ee7da163849885839b988823f711
Thiago Alves <thiagosales06@hotmail.com>
2023-09-11 20:54:08 -0300

    Usando restrições genericas

---
+++ C#/RestricaoGenerics/RestricaoGenerics/Program.cs
@@ -0,0 +1,31 @@
+﻿using RestricaoGenerics.Entities;
+using RestricaoGenerics.Services;
+
+namespace RestricaoGenerics
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Product> list = new List<Product>();
+
+            Console.Write("Enter N: ");
+            int n = int.Parse(Console.ReadLine());
+
+            for(int i = 0; i < n; i++)
+            {
+                string[] vect = Console.ReadLine().Split(',');
+                string name = vect[0];
+                double price = double.Parse(vect[1]);
+                list.Add(new Product(name, price));
+            }
+
+             CalculationService calculationService = new CalculationService();
+
+            Product max = calculationService.Max(list);
+
+            Console.WriteLine("MAX:");
+            Console.WriteLine(max.ToString());
+        }
+    }
+}
