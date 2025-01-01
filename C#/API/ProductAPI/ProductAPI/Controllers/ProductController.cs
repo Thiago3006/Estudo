ba1fef6b500db4b6fe90d51342820a4a9f43ed38
Thiago Alves <thiagosales06@hotmail.com>
2025-01-01 13:58:37 -0300

    Revisando Api, utilizando vs para commit do ano.

---
+++ C#/API/ProductAPI/ProductAPI/Controllers/ProductController.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ProductAPI.Controllers
+{
+    [ApiController]
+    [Route("api/demo")]
+    public class ProductController : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult Get()
+        {
+            return Ok(new Product { Id = 1, Name = "Paçoca" });
+        }
+
+        [HttpGet("{id:int}")]
+        public IActionResult Get(int id)
+        {
+            return Ok(new Product { Id = id, Name = "Paçoca" });
+        }
+
+        [HttpPost]
+        public IActionResult Post(Product product)
+        {
+            return CreatedAtAction("Get", new { id = product.Id }, product);
+        }
+
+        [HttpPut("{id}")]
+        public IActionResult Put(int id, Product product)
+        {
+            if (id != product.Id) return BadRequest();
+            return NoContent();
+        }
+
+        [HttpDelete("{id:int}")]
+        public IActionResult Delete(int id)
+        {
+            return NoContent();
+        }
+    }
+}
