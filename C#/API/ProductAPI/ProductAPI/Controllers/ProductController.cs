d5f71d00a44373eb8baebab0f3a725d81e2c4006
Thiago Alves <thiagosales06@hotmail.com>
2025-01-03 20:40:03 -0300

    Adicionado ProducesResponse  e  removido classe weatherForecast

--- C#/API/ProductAPI/ProductAPI/Controllers/ProductController.cs
+++ C#/API/ProductAPI/ProductAPI/Controllers/ProductController.cs
@@ -7,24 +7,31 @@ namespace ProductAPI.Controllers
     public class ProductController : ControllerBase
     {
         [HttpGet]
+        [ProducesResponseType(typeof(Product), StatusCodes.Status200OK)]
         public IActionResult Get()
         {
             return Ok(new Product { Id = 1, Name = "Paçoca" });
         }
 
         [HttpGet("{id:int}")]
+        [ProducesResponseType(typeof(Product), StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
         public IActionResult Get(int id)
         {
             return Ok(new Product { Id = id, Name = "Paçoca" });
         }
 
         [HttpPost]
+        [ProducesResponseType(typeof(Product),StatusCodes.Status201Created)]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
         public IActionResult Post(Product product)
         {
             return CreatedAtAction("Get", new { id = product.Id }, product);
         }
 
         [HttpPut("{id}")]
+        [ProducesResponseType(StatusCodes.Status204NoContent)]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
         public IActionResult Put(int id, Product product)
         {
             if (id != product.Id) return BadRequest();
@@ -32,8 +39,12 @@ namespace ProductAPI.Controllers
         }
 
         [HttpDelete("{id:int}")]
+        [ProducesResponseType(StatusCodes.Status204NoContent)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+        [ProducesDefaultResponseType]
         public IActionResult Delete(int id)
         {
+            if (id == 0) return NotFound();
             return NoContent();
         }
     }
