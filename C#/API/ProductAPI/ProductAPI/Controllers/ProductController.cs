307a3289f5569c74bb4e4387e1975150dc1d9f89
Thiago Alves <thiagosales06@hotmail.com>
2025-01-07 20:46:27 -0300

    feature: Controller realiza o crud via banco na tabela products

--- C#/API/ProductAPI/ProductAPI/Controllers/ProductController.cs
+++ C#/API/ProductAPI/ProductAPI/Controllers/ProductController.cs
@@ -12,21 +12,35 @@ namespace ProductAPI.Controllers
     {
 
         private readonly ProductApiDbContext _context;
-        public ProductController(ProductApiDbContext context) {
+        public ProductController(ProductApiDbContext context)
+        {
             _context = context;
         }
 
         [HttpGet]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+        [ProducesDefaultResponseType]
         public async Task<ActionResult<IEnumerable<Product>>> GetProducts()
         {
+            if (_context.Products == null)
+            {
+                return NotFound();
+            }
+
             return await _context.Products.ToListAsync();
         }
 
         [HttpGet("{id:int}")]
         [ProducesResponseType(StatusCodes.Status200OK)]
-        [ProducesResponseType(StatusCodes.Status404NotFound)]        
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
         public async Task<ActionResult<Product>> GetProduct(int id)
         {
+            if (_context.Products == null)
+            {
+                return NotFound();
+            }
+
             var product = await _context.Products.FindAsync(id);
             if (product == null)
             {
@@ -40,6 +54,16 @@ namespace ProductAPI.Controllers
         [ProducesResponseType(StatusCodes.Status400BadRequest)]
         public async Task<ActionResult<Product>> PostProduct(Product product)
         {
+            if (_context.Products == null)
+            {
+                return Problem("Erro ao criar.");
+            }
+
+            if (!ModelState.IsValid)
+            {
+                return ValidationProblem(ModelState);
+            }
+
             _context.Products.Add(product);
             await _context.SaveChangesAsync();
             return CreatedAtAction("GetProduct", new { id = product.Id }, product);
@@ -48,15 +72,35 @@ namespace ProductAPI.Controllers
         [HttpPut("{id:int}")]
         [ProducesResponseType(StatusCodes.Status204NoContent)]
         [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+        [ProducesDefaultResponseType]
         public async Task<IActionResult> PutProduct(int id, Product product)
         {
             if (id != product.Id)
             {
                 return BadRequest();
             }
-            //_context.Entry(product).State = EntityState.Modified;
-            _context.Products.Update(product);
-            await _context.SaveChangesAsync();
+
+
+            _context.Entry(product).State = EntityState.Modified;
+
+            try
+            {
+                await _context.SaveChangesAsync();
+
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!ProductExists(id)) 
+                { 
+                    return NotFound(); 
+                }
+                else
+                {
+                    throw;
+                }
+
+            }
             return NoContent();
         }
 
@@ -65,7 +109,14 @@ namespace ProductAPI.Controllers
         [ProducesResponseType(StatusCodes.Status404NotFound)]
         public async Task<IActionResult> DeleteProduct(int id)
         {
+
+            if (_context.Products == null)
+            {
+                return NotFound();
+            }
+
             var product = await _context.Products.FindAsync(id);
+            
             if (product == null)
             {
                 return NotFound();
@@ -73,6 +124,11 @@ namespace ProductAPI.Controllers
             _context.Products.Remove(product);
             await _context.SaveChangesAsync();
             return NoContent();
-        }        
+        }
+
+        private bool ProductExists(int id)
+        {
+            return (_context.Products?.Any(e => e.Id == id)).GetValueOrDefault();
+        }
     }
 }
