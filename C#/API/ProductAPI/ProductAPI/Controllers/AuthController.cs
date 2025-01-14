faef05ab72f1d44988fdf972f950dd324a43b097
Thiago Alves <thiagosales06@hotmail.com>
2025-01-14 20:30:02 -0300

    feature: Iniciando controller de autenticação

---
+++ C#/API/ProductAPI/ProductAPI/Controllers/AuthController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ProductAPI.Controllers
+{
+    [ApiController]
+    [Route("api/account")]
+    public class AuthController : ControllerBase
+    {
+        [HttpPost("register")]
+        public async Task<ActionResult> Register()
+        {           
+            return Ok();
+        }
+
+        [HttpPost("login")]
+        public async Task<ActionResult> Login()
+        {
+            return Ok();
+        }
+    }
+}
