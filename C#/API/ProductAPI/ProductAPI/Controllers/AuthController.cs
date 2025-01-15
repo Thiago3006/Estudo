c5e9d5ad9bcb28bc43ec2e7e76e05abe7081ee47
Thiago Alves <thiagosales06@hotmail.com>
2025-01-15 19:38:52 -0300

    feature: implementando controller de autenticação.

--- C#/API/ProductAPI/ProductAPI/Controllers/AuthController.cs
+++ C#/API/ProductAPI/ProductAPI/Controllers/AuthController.cs
@@ -1,4 +1,7 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Options;
+using ProductAPI.Models;
 
 namespace ProductAPI.Controllers
 {
@@ -6,14 +9,47 @@ namespace ProductAPI.Controllers
     [Route("api/account")]
     public class AuthController : ControllerBase
     {
+        private readonly SignInManager<IdentityUser> _signInManager;
+        private readonly UserManager<IdentityUser> _userManager;
+        private readonly JwtSettings _jwtSettings;
+
+        public AuthController(SignInManager<IdentityUser> signInManager, 
+            UserManager<IdentityUser> userManager, 
+            IOptions<JwtSettings> jwtSettings)
+        {
+            _signInManager = signInManager;
+            _userManager = userManager;
+            _jwtSettings = jwtSettings.Value;
+        }
+
         [HttpPost("register")]
-        public async Task<ActionResult> Register()
+        public async Task<ActionResult> Register(RegisterUserViewModel registerUser)
         {           
-            return Ok();
+            if  (!ModelState.IsValid)
+            {
+                return ValidationProblem(ModelState);
+            }
+
+            var user = new IdentityUser
+            {
+                UserName = registerUser.Email,
+                Email = registerUser.Email,
+                EmailConfirmed = true
+            };
+
+            var result = await _userManager.CreateAsync(user, registerUser.Password);
+            if (result.Succeeded)
+            {
+                await _signInManager.SignInAsync(user, false);
+                return Ok();
+            }
+
+            return Problem("Falha ao cadastrar usuário", null, 500);
+
         }
 
         [HttpPost("login")]
-        public async Task<ActionResult> Login()
+        public async Task<ActionResult> Login(LoginUserViewModel loginUser)
         {
             return Ok();
         }
